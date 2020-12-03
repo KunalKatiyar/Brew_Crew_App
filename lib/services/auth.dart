50bc13711f2d9a16dee5e617aa1ef5888342cae7
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-12-03 22:11:12 +0530

    first commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,62 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  NewUser _userFromFirebaseUser(User user) {
+    return user != null ? NewUser(uid: user.uid) : null;
+  }
+
+  Stream<NewUser> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+    //.map((User user) => _userFromFirebaseUser(user));
+  }
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return user;
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'New Crew Member', 100);
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+}
---
+++ lib/services/database.dart
@@ -0,0 +1,44 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+  final CollectionReference brewCollection =
+      FirebaseFirestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.doc(uid).set({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid,
+        name: snapshot.data()['name'],
+        sugars: snapshot.data()['sugars'],
+        strength: snapshot.data()['strength']);
+  }
+
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+        name: doc.data()['name'] ?? '',
+        strength: doc.data()['strength'] ?? 0,
+        sugars: doc.data()['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
+}
